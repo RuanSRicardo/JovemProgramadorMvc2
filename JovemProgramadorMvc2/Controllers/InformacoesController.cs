b316d5dad58e55321dc43d35a6a3fbec27153fbb
RUAN SECATI RICARDO <ruan.ricardo@alunos.sc.senac.br>
2022-06-10 19:47:36 -0300

    10/06/2022 aba nova

---
+++ JovemProgramadorMvc2/Controllers/InformacoesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JovemProgramadorMvc2.Controllers
+{
+    public class InformacoesController : Controller
+    {
+        public InformacoesController()
+        {
+
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
